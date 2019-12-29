07cc09e18fc07d864f9593754e9109ff36ca81a8
jiefeng <jiefengfire@163.com>
2019-12-29 17:37:34 +0800

    aaa

--- src/main/java/com/jiefeng/imagesbed/ImagesbedApplication.java
+++ src/main/java/com/jiefeng/imagesbed/ImagesbedApplication.java
@@ -11,11 +11,8 @@ package com.jiefeng.imagesbed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 
 @SpringBootApplication
-@EnableSwagger2
 @ComponentScan(basePackages = {"com.definesys.mpaas","com.jiefeng.imagesbed"})
 public class ImagesbedApplication {
 
